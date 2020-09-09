c2e583a8e7a81b9a45f2781e09bd5fd2c3208369
Kenneth L. Hansen <klh@minuba.dk>
2020-09-09 17:09:43 +0200

    addedRemvoeTrailingZeroes method

--- src/main/java/romanNumeralConverter/RomanNumeralConverter.java
+++ src/main/java/romanNumeralConverter/RomanNumeralConverter.java
@@ -3,11 +3,82 @@ package romanNumeralConverter;
 public class RomanNumeralConverter {
     public String convert(int number) {
         String romanNumber = "";
-        for (int i = 0; i < number; i++) {
-            if (i < 4)
-                romanNumber += "I";
-            if (i == 5)
-                romanNumber += "V";
+        if (number < 3999) {
+            for (int i = number; i > 0; i--) {
+
+                if (i > 999 && i < 4000) {
+                    romanNumber += "M";
+                    i = i - 999;
+                    continue;
+                }
+
+                if (i > 899 && i < 1000) {
+                    romanNumber += "CM";
+                    i = i - 899;
+                    continue;
+                }
+
+                if (i > 499 && i < 900) {
+                    romanNumber += "D";
+                    i = i - 499;
+                    continue;
+                }
+
+                if (i > 399 && i < 500) {
+                    romanNumber += "CD";
+                    i = i - 399;
+                    continue;
+                }
+
+                if (i > 99 && i < 400) {
+                    romanNumber += "C";
+                    i = i - 99;
+                    continue;
+                }
+
+                if (i > 89 && i < 100) {
+                    romanNumber += "XC";
+                    i = i - 89;
+                    continue;
+                }
+
+                if (i > 49 && i < 90) {
+                    romanNumber += "L";
+                    i = i - 49;
+                    continue;
+                }
+
+                if (i > 39 && i < 50) {
+                    romanNumber += "XL";
+                    i = i - 39;
+                    continue;
+                }
+
+                if (i > 9 && i < 40) {
+                    romanNumber += "X";
+                    i = i - 9;
+                    continue;
+                }
+                if (i == 9) {
+                    romanNumber += "IX";
+                    i = i - 8;
+                    continue;
+                }
+                if (i > 4 && i < 9) {
+                    romanNumber += "V";
+                    i = i - 4;
+                    continue;
+                }
+                if (i == 4) {
+                    romanNumber += "IV";
+                    i = i - 3;
+                    continue;
+                }
+                if (i < 4) {
+                    romanNumber += "I";
+                    continue;
+                }
+            }
         }
         return romanNumber;
     }
--- src/test/java/romanNumeralConverter/RomanNumeralConverterTest.java
+++ src/test/java/romanNumeralConverter/RomanNumeralConverterTest.java
@@ -3,17 +3,16 @@ package romanNumeralConverter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RomanNumeralConverterTest {
 
     @Test
-   public void mustBeAbleToCreateConverter(){
+    public void mustBeAbleToCreateConverter() {
         //Arrange
 
-       //Act
+        //Act
         RomanNumeralConverter romanConverter = new RomanNumeralConverter();
 
         //Assert
@@ -21,7 +20,7 @@ public class RomanNumeralConverterTest {
     }
 
     @Test
-    public void convertOneToI(){
+    public void convertOneToI() {
         //Arrange
         RomanNumeralConverter romanConverter = new RomanNumeralConverter();
         String expectedResult = "I";
@@ -61,5 +60,215 @@ public class RomanNumeralConverterTest {
         assertEquals(expectedResult, actualResult);
 
     }
+
+    @Test
+    public void convertSixToVI() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "VI";
+
+        //Act
+        String actualResult = romanConverter.convert(6);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertEightToVIII() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "VIII";
+
+        //Act
+        String actualResult = romanConverter.convert(8);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertNineTo9() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "IX";
+
+        //Act
+        String actualResult = romanConverter.convert(9);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertTenToX() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "X";
+
+        //Act
+        String actualResult = romanConverter.convert(10);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertThirteenToXIII() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "XIII";
+
+        //Act
+        String actualResult = romanConverter.convert(13);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertNineteenToXIX() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "XIX";
+
+        //Act
+        String actualResult = romanConverter.convert(19);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+
+    @Test
+    public void convertFortiesToXL() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "XLII";
+
+        //Act
+        String actualResult = romanConverter.convert(42);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertFiftiesToL() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "LXV";
+
+        //Act
+        String actualResult = romanConverter.convert(65);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertNineTeesToXC() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "XCIV";
+
+        //Act
+        String actualResult = romanConverter.convert(94);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertHundredsToC() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "CCLIX";
+
+        //Act
+        String actualResult = romanConverter.convert(259);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertFourHundredsToCD() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "CDXXXIII";
+
+        //Act
+        String actualResult = romanConverter.convert(433);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+
+    @Test
+    public void convertFiveHundredsToD() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "DCCCXCII";
+
+        //Act
+        String actualResult = romanConverter.convert(892);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertNineHundredsToCM() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "CMXCIX";
+
+        //Act
+        String actualResult = romanConverter.convert(999);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void convertThousandsToM() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        String expectedResult = "MMMCCCLII";
+
+        //Act
+        String actualResult = romanConverter.convert(3352);
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void EmptyIfAbove3999() {
+        //Arrange
+        RomanNumeralConverter romanConverter = new RomanNumeralConverter();
+        //Act
+        String actualResult = romanConverter.convert(4555);
+
+        //Assert
+        assertEquals("", actualResult);
+    }
+
 
 }
